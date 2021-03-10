69202e85fe210b6064eb77d1a4e9f281d0a58f2b
jimbucktoo <jamesliang.g@gmail.com>
2021-03-10 12:20:36 -0600

    critical syntax correction

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,36 +1,36 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.17
 
 contract Lottery {
-    address public manager;
-    address[] public players;
+    address public manager
+    address[] public players
 
     function Lottery() public {
-        manager = msg.sender;
+        manager = msg.sender
     }
 
     function enter() public payable {
-        require(msg.value >= 10000000 wei);
-        players.push(msg.sender);
+        require(msg.value >= 10000000 wei)
+        players.push(msg.sender)
     }
 
     function random() public view returns (uint) {
-        //sha3();
-        return uint(keccak256(block.difficulty, now, players));
+        //sha3()
+        return uint(keccak256(block.difficulty, now, players))
     }
 
     function pickWinner() public restricted{
-        require(msg.sender == manager);
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+        require(msg.sender == manager)
+        uint index = random() % players.length
+        players[index].transfer(this.balance)
+        players = new address[](0)
     }
 
     modifier restricted() {
-        require(msg.sender == manager);
-        _;
+        require(msg.sender == manager)
+        _
     }
 
     function getPlayers() public view returns (address[]) {
-        return players;
+        return players
     }
 }
