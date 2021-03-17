fbd872f86e0d9c19fbd0f0e50384834c4910d0ba
jimbucktoo <jamesliang.g@gmail.com>
2021-03-16 19:12:40 -0500

    critical lastWinner variable declaration correction

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.17;
 contract Lottery {
     address public manager;
     address[] public players;
+    address public lastWinner;
 
     function Lottery() public {
         manager = msg.sender;
